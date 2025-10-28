1ebb085a244c34df4a2c81e010425eac7236344c
Joel <105253436+JoelLovesKFC@users.noreply.github.com>
2025-10-28 06:00:09 +0000

    created both register and login in page with css, note: redirect login to dashboard, add vallidation errors, datadb for ids and allowing the user to update username,email or password but keeps that unqiue id number

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
+def home(): 
     return render_template("login.html")
 
-
-app.route("/login", methods = ["GET", "POST"])
-def login(): 
-    if request.method == "POST": 
-        username = request.form["username"]
-        password = request.form["password"]
-    
+@app.route("/login")
+def login():
     return render_template("login.html")
 
+@app.route("/register")
+def register(): 
+    return render_template("register.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 
 if __name__ == '__main__':
