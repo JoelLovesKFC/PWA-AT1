f3c4fef46e6e44c4918b46fd131b2ce4d3215a4d
Joel <105253436+JoelLovesKFC@users.noreply.github.com>
2025-10-29 01:26:28 +0000

    added success button

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
 import sqlite3
 
 app = Flask(__name__)
@@ -6,19 +6,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def home(): 
-    return render_template("login.html")
+    return render_template()
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-@app.route("/register")
-def register(): 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
     return render_template("register.html")
+    # if request.method == "POST":
+    #     flash("Registration successful!")
+    #     return render_template("register.html", success=True)
+    # return render_template("register.html", success=False)
+
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    pass
 
 
 if __name__ == '__main__':
