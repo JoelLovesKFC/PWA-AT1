8bf42a05cd1b20c2cb97e5bb3af843d0ef571470
Joel <105253436+JoelLovesKFC@users.noreply.github.com>
2025-10-29 02:56:06 +0000

    remove comments and added name input

--- main.py
+++ main.py
@@ -16,15 +16,16 @@ def login():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html")
-    # if request.method == "POST":
-    #     flash("Registration successful!")
-    #     return render_template("register.html", success=True)
-    # return render_template("register.html", success=False)
+
 
 
 @app.route("/dashboard")
 def dashboard():
     pass
+
+
+
+
 
 
 if __name__ == '__main__':
